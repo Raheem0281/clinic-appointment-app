75480822d96972ac386498b51210aa5948e7bb97
Abdul Raheem <rr867535@gmail.com>
2025-07-19 07:36:26 +0500

    Final fix: STATIC_ROOT and Whitenoise for Railway deployment

--- clinicproject/settings.py
+++ clinicproject/settings.py
@@ -108,6 +108,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
